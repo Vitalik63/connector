1a6c42c888fcce1c9183608b30195930d1353a84
Vitalik Morozov <morozovitalick@gmail.com>
2024-12-27 15:04:51 +0300

    Update Connector font and modify nacs icon data in connector_icons.dart

--- lib/connector_icons.dart
+++ lib/connector_icons.dart
@@ -12,10 +12,10 @@ class ConnectorIcons {
   static const String _kFontFam = 'Connector';
   static const String _kFontPkg = 'connector_icons';
 
+  static const IconData nacs = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData type_1 = IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData type_2 = IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData ccs = IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData chademo = IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData gbt = IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData nacs = IconData(0xe82f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
